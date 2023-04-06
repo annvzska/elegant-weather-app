b63366d84a42b9559ee6a060940b6cbeefac29c7
annvzska <annvzska@gmail.com>
2023-04-06 18:52:58 +0200

    Changed showTemp function.

--- src/index.js
+++ src/index.js
@@ -87,6 +87,7 @@ function showTemp(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   
   celsiusTemperature = response.data.main.temp;
